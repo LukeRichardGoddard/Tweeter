d8c4afce6377fb9f9ce872b2182977a171facad8
Luke Richard Goddard <github@lukerichardgoddard.com>
2024-06-08 15:53:46 +1000

    initial commit

---
+++ Tweeter/TweeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TweeterApp.swift
+//  Tweeter
+//
+//  Created by Luke Goddard on 8/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct TweeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
