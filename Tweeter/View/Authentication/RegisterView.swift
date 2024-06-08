f532d5a27d60a830c4c13c1c479ff12f6e508d43
Luke Richard Goddard <github@lukerichardgoddard.com>
2024-06-09 03:13:12 +1000

    add register view

---
+++ Tweeter/View/Authentication/RegisterView.swift
@@ -0,0 +1,77 @@
+//
+//  RegisterView.swift
+//  Tweeter
+//
+//  Created by Luke Goddard on 9/6/2024.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    
+    @State var name = ""
+    @State var email = ""
+    @State var password = ""
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                HStack {
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Cancel")
+                            .foregroundColor(.blue)
+                    })
+                    Spacer()
+                }
+                .padding(.horizontal)
+                
+                Image("Twitter")
+                    .resizable()
+                    .scaledToFill()
+                    .padding(.trailing)
+                    .frame(width: 20, height: 20)
+            }
+            
+            Text("Create your account")
+                .font(.title)
+                .bold()
+                .padding(.top, 25)
+            
+            VStack(alignment: .leading, spacing: nil) {
+                CustomAuthTextField(placeholder: "Name", text: $name)
+                CustomAuthTextField(placeholder: "Phone Number or Email", text: $email)
+                SecureAuthTextField(placeholder: "Password", text: $password)
+            }
+            
+            Spacer(minLength: 0)
+            
+            VStack {
+                Rectangle()
+                    .frame(height: 1)
+                    .foregroundColor(.gray)
+                
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Capsule()
+                            .frame(width: 60, height: 30, alignment: .center)
+                            .foregroundColor(Color(red: 29 / 255, green: 161 / 255, blue: 242 / 255))
+                            .overlay(
+                                Text("Next")
+                                    .foregroundColor(.white))
+                    })
+                    .padding(.trailing, 24)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    RegisterView()
+}
---
+++ Tweeter/View/Items/SecureAuthTextField.swift
@@ -0,0 +1,33 @@
+//
+//  SecureAuthTextField.swift
+//  Tweeter
+//
+//  Created by Luke Goddard on 9/6/2024.
+//
+
+import SwiftUI
+
+struct SecureAuthTextField: View {
+    var placeholder: String = "Placeholder"
+    @Binding var text: String
+    
+    var body: some View {
+        VStack {
+            ZStack(alignment: .leading) {
+                if text.isEmpty {
+                    Text(placeholder)
+                        .foregroundColor(.gray)
+                }
+                SecureField("", text: $text)
+                    .frame(height: 45)
+                    .foregroundColor(Color(red: 29 / 255, green: 161 / 255, blue: 242 / 255))
+            }
+            
+            Rectangle()
+                .frame(height: 1, alignment: .center)
+                .foregroundColor(.gray)
+                .padding(.top, -2)
+        }
+        .padding(.horizontal)
+    }
+}
