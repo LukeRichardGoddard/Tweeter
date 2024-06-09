7614a7b2679803a05fb761cd3dee1fb7c414d14b
Luke Richard Goddard <github@lukerichardgoddard.com>
2024-06-10 03:03:23 +1000

    add auth viewmodel and user model

---
+++ Tweeter/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  Tweeter
+//
+//  Created by Luke Goddard on 10/6/2024.
+//
+
+import Foundation
+
+struct User: Identifiable, Codable {
+    var _id: String
+    var id: String { return _id }
+    let username: String
+    let name: String
+    let email: String
+    var location: String?
+    var bio: String?
+    var website: String?
+    var avatarExists: Bool
+    var followers: [String]
+    var following: [String]
+}
---
+++ Tweeter/ViewModel/AuthViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  AuthViewModel.swift
+//  Tweeter
+//
+//  Created by Luke Goddard on 10/6/2024.
+//
+
+import SwiftUI
+
+class AuthViewModel: ObservableObject {
+    
+    func login() {
+        
+    }
+    
+    func register() {
+        
+    }
+    
+    func logout() {
+        
+    }
+    
+}
