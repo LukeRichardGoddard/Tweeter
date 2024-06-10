5b66fecd77861bad29708cbf3e6b9ee953b9f726
Luke Richard Goddard <github@lukerichardgoddard.com>
2024-06-11 02:48:47 +1000

    update feed view model

--- Tweeter/ViewModel/Feed/FeedViewModel.swift
+++ Tweeter/ViewModel/Feed/FeedViewModel.swift
@@ -7,4 +7,27 @@
 
 import SwiftUI
 
+class FeedViewModel: ObservableObject {
+    
+    @Published var tweets = [Tweet]()
+    
+    func fetchTweets() {
+        
+        RequestServices.requestDomain = "\(K.Network.server)/tweets"
+        
+        RequestServices.fetchTweets { result in
+            switch result {
+                case .success(let data):
+                    guard let tweets = try? JSONDecoder().decode([Tweet].self, from: data as! Data) else { return }
+                    DispatchQueue.main.async {
+                        self.tweets = tweets
+                    }
+                case .failure(let error):
+                    print(error.localizedDescription)
+            }
+        }
+        
+    }
+    
+}
 
